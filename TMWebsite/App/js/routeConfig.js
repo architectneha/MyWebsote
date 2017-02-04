bf0ee15e7fecdd0bd016bd998b4dc1f0f7aa98f4
evya <evya@EVYAPC>
2017-02-04 15:24:10 +0200

    Adding route config and pages

---
+++ TMWebsite/App/js/routeConfig.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="angular-route.js" />
+/// <reference path="tmwapp.js" />
+
+tmwapp.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "../Pages/HomePage.html",
+        controller: "../Controllers/HomeController.js"
+    })
+    .when("/Login", {
+        templateUrl: "../Pages/LoginPage.aspx",
+        controller: "../Controllers/LoginController.js"
+    })
+    .otherwise({
+        redirectTo : "/"
+    });
+});
