11186eefd51fbbd22fd79db5308a35d7190de487
evya <evya@EVYAPC>
2017-02-04 15:17:32 +0200

    Adding js css fonts and index

---
+++ TMWebsite/App/Controllers/HomeController.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="../js/angular.js" />
+/// <reference path="../js/angular-route.js" />
+/// <reference path="../js/tmwapp.js" />
+
+tmwapp.controller("HomeController", ['$scope', '$routeParams', function ($scope) {
+    $scope.message = "Hello";
+}]);
